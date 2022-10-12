e6de37e2e76e427c5ec68227eb80b05b61884ec5
Georgi Angelov <gangelov@asteasolutions.com>
2022-10-12 13:19:15 +0300

    Remove incorrect TODO

--- spec/modifiers/optional.spec.ts
+++ spec/modifiers/optional.spec.ts
@@ -4,14 +4,12 @@ import { expectSchema } from '../lib/helpers';
 describe('optional', () => {
   it('generates OpenAPI schema for optional after the metadata', () => {
     expectSchema([z.string().openapi({ refId: 'SimpleString' }).optional()], {
-      // TODO: Um, where is the optional?
       SimpleString: { type: 'string' },
     });
   });
 
   it('generates OpenAPI schema for optional before the metadata', () => {
     expectSchema([z.string().optional().openapi({ refId: 'SimpleString' })], {
-      // TODO: Um, where is the optional?
       SimpleString: { type: 'string' },
     });
   });
